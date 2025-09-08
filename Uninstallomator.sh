75d8b55dba70f0c797c0de77c8a2f3a06e7cbed1
Robert Schroeder <robjschroeder@gmail.com>
2025-09-08 08:28:59 -0600

    1.1.1
    Added updated labels to main build

--- Uninstallomator.sh
+++ Uninstallomator.sh
@@ -490,6 +490,26 @@ googlechrome)
     daemons=()
     profiles=()
 ;;
+microsoftcompanyportal)
+    app_name="Company Portal"
+    bundle_id="com.microsoft.CompanyPortalMac"
+    app_paths=(
+      "/Applications/Company Portal.app"
+    )
+    pkgs=(
+      "com.microsoft.CompanyPortalMac"
+    )
+    files=()
+    user_files=(
+      "%USER_HOME%/Library/Application Support/Company Portal"
+      "%USER_HOME%/Library/Preferences/com.microsoft.CompanyPortalMac.plist"
+      "%USER_HOME%/Library/Caches/com.microsoft.CompanyPortalMac"
+      "%USER_HOME%/Library/Logs/Company Portal"
+    )
+    agents=()
+    daemons=()
+    profiles=()
+;;
 microsoftedge)
     app_name="Edge"
     bundle_id="com.microsoft.edgemac"
@@ -498,21 +518,6 @@ microsoftedge)
     )
     pkgs=(
       "com.microsoft.edgemac"
-      "com.microsoft.dlp.ux"
-      "com.microsoft.dlp.daemon"
-      "com.jamf.appinstallers.Edge"
-      "com.microsoft.package.Microsoft_Excel.app"
-      "com.microsoft.powershell"
-      "com.microsoft.CompanyPortalMac"
-      "com.microsoft.OneDrive"
-      "com.microsoft.wdav"
-      "com.microsoft.package.Microsoft_Outlook.app"
-      "com.microsoft.dlp.agent"
-      "com.jamf.appinstallers.MicrosoftEdge"
-      "com.microsoft.package.Microsoft_AutoUpdate.app"
-      "com.microsoft.MSTeamsAudioDevice"
-      "com.microsoft.pkg.licensing"
-      "com.microsoft.teams2"
     )
     files=(
       "/Library/microsoft/Edge"

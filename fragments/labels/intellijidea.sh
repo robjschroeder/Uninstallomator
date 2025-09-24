537531303a53921e50cc59c1d028532e736d3326
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-09-24 15:53:48 -0400

    New Label: IntelliJ IDEA
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

---
+++ fragments/labels/intellijidea.sh
@@ -0,0 +1,18 @@
+intellijidea)
+    app_name="IntelliJ IDEA"
+    bundle_id="com.jetbrains.intellij"
+    app_paths=(
+      "/Applications/IntelliJ IDEA.app"
+    )
+    pkgs=()
+    files=()
+    user_files=(
+      "%USER_HOME%/Library/Application Support/IntelliJ IDEA"
+      "%USER_HOME%/Library/Preferences/com.jetbrains.intellij.plist"
+      "%USER_HOME%/Library/Caches/com.jetbrains.intellij"
+      "%USER_HOME%/Library/Logs/IntelliJ IDEA"
+    )
+    agents=()
+    daemons=()
+    profiles=()
+;;

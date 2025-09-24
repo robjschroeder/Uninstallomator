0252a523fdca4ddbd9e052d4939ebd48639c4048
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-09-24 16:36:18 -0400

    Updated Label: IntelliJ IDEA
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- fragments/labels/intellijidea.sh
+++ fragments/labels/intellijidea.sh
@@ -8,6 +8,7 @@ intellijidea)
     files=()
     user_files=(
       "%USER_HOME%/Library/Application Support/IntelliJ IDEA"
+      "%USER_HOME%/Library/Application Support/JetBrains"
       "%USER_HOME%/Library/Preferences/com.jetbrains.intellij.plist"
       "%USER_HOME%/Library/Caches/com.jetbrains.intellij"
       "%USER_HOME%/Library/Logs/IntelliJ IDEA"

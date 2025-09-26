aac9252271804bc985fdbbd410c1fcf3c7d0f4ed
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-09-26 07:17:54 -0400

    New Label: Whisper Transcription
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

---
+++ fragments/labels/whispertranscription.sh
@@ -0,0 +1,20 @@
+whispertranscription)
+    app_name="Whisper Transcription"
+    bundle_id="com.goodsnooze.MacWhisper"
+    app_paths=(
+      "/Applications/Whisper Transcription.app"
+    )
+    pkgs=(
+      "com.goodsnooze.MacWhisper"
+    )
+    files=()
+    user_files=(
+      "%USER_HOME%/Library/Application Support/Whisper Transcription"
+      "%USER_HOME%/Library/Preferences/com.goodsnooze.MacWhisper.plist"
+      "%USER_HOME%/Library/Caches/com.goodsnooze.MacWhisper"
+      "%USER_HOME%/Library/Logs/Whisper Transcription"
+    )
+    agents=()
+    daemons=()
+    profiles=()
+;;
